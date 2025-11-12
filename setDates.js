39f08384c823092ba2e66ae0e8a4f239e78770bd
martinezdylanok <dm97fx@gmail.com>
2025-11-12 10:26:10 +0100

    feat: integrate Biome for formatting and linting; add biome.json and jsconfig.json; remove ESLint configuration

--- setDates.js
+++ setDates.js
@@ -1,18 +1,15 @@
 const setDates = () => {
+	const now = new Date();
 
-  const now = new Date();
+	const startDate = new Date(
+		Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1, 0, 0, 0, 0),
+	).toISOString();
 
-  const startDate = new Date(Date.UTC(
-    now.getUTCFullYear(), now.getUTCMonth(), 1, 0, 0, 0, 0
-  )).toISOString();
+	const endDate = new Date(
+		Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, 0, 23, 59, 59, 999),
+	).toISOString();
 
-  const endDate = new Date(Date.UTC(
-    now.getUTCFullYear(), now.getUTCMonth() + 1, 0, 23, 59, 59, 999
-  )).toISOString();
-
-  return { now, startDate, endDate };
-
-}
+	return { now, startDate, endDate };
+};
 
 export default setDates;
-
--- setPostRequest.js
+++ setPostRequest.js
@@ -1,14 +1,15 @@
 const setPostRequest = (searchUrl, originUrl, refererUrl, body) => {
-   return () => fetch(searchUrl, {
-      method: "POST",
-      headers: {
-         'Accept': "application/json",
-         'Content-Type': "application/json",
-         'Origin': `${originUrl}`,
-         'Referer': `${refererUrl}`,
-      },
-      body: JSON.stringify(body),
-   });
+	return () =>
+		fetch(searchUrl, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Origin: `${originUrl}`,
+				Referer: `${refererUrl}`,
+			},
+			body: JSON.stringify(body),
+		});
 };
 
 export default setPostRequest;
